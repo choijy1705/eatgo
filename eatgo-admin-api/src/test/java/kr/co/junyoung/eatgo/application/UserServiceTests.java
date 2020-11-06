ff50e2eb310b28ba8c641bb647e237ce4883247d
choijy1705 <cjy1705@naver.com>
2020-11-07 00:01:20 +0900

    User 생성

---
+++ eatgo-admin-api/src/test/java/kr/co/junyoung/eatgo/application/UserServiceTests.java
@@ -0,0 +1,51 @@
+package kr.co.junyoung.eatgo.application;
+
+import kr.co.junyoung.eatgo.domain.CategoryRepository;
+import kr.co.junyoung.eatgo.domain.User;
+import kr.co.junyoung.eatgo.domain.UserRepository;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+class UserServiceTests {
+
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    public void getUsers(){
+
+        List<User> mockUsers = new ArrayList<>();
+        mockUsers.add(User.builder()
+                .email("tester@example.com")
+                .name("tester")
+                .level(1L)
+                .build());
+        given(userRepository.findAll()).willReturn(mockUsers);
+
+        List<User> users = userService.getUsers();
+
+        User user = users.get(0);
+        assertThat(user.getName(), is("tester"));
+    }
+
+}
---
+++ eatgo-common/src/test/java/kr/co/junyoung/eatgo/domain/UserTests.java
@@ -0,0 +1,23 @@
+package kr.co.junyoung.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTests {
+
+    @Test
+    public void creation(){
+        User user = User.builder()
+                .email("tester@example.com")
+                .name("테스터")
+                .level(3L)
+                .build();
+
+        assertThat(user.getName(), is("테스터"));
+        assertThat(user.isAdmin(), is(true));
+    }
+
+}
